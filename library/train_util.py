9ead6bdea0f9ccaa76d147dbbb30876f36125683
Diodotos <165386228+diodotosml@users.noreply.github.com>
2024-04-04 19:43:03 +0200

    Stuff

--- library/train_util.py
+++ library/train_util.py
@@ -4934,15 +4934,17 @@ def save_sd_model_on_train_end_common(
             huggingface_util.upload(args, out_dir, "/" + model_name, force_sync_upload=True)
 
 
-def get_noise_noisy_latents_and_timesteps(args, noise_scheduler, latents, timesteps = None):
+def get_noise_noisy_latents_and_timesteps(args, noise_scheduler, latents, timesteps = None, noise = None, noise_offset = None):
     # Sample noise that we'll add to the latents
-    noise = torch.randn_like(latents, device=latents.device)
-    if args.noise_offset:
-        if args.noise_offset_random_strength:
-            noise_offset = torch.rand(1, device=latents.device) * args.noise_offset
-        else:
-            noise_offset = args.noise_offset
-        noise = custom_train_functions.apply_noise_offset(latents, noise, noise_offset, args.adaptive_noise_scale)
+    if not noise:
+        noise = torch.randn_like(latents, device=latents.device)
+    if not noise_offset:
+        if args.noise_offset:
+            if args.noise_offset_random_strength:
+                noise_offset = torch.rand(1, device=latents.device) * args.noise_offset
+            else:
+                noise_offset = args.noise_offset
+            noise = custom_train_functions.apply_noise_offset(latents, noise, noise_offset, args.adaptive_noise_scale)
     if args.multires_noise_iterations:
         noise = custom_train_functions.pyramid_noise_like(
             noise, latents.device, args.multires_noise_iterations, args.multires_noise_discount
@@ -4969,7 +4971,7 @@ def get_noise_noisy_latents_and_timesteps(args, noise_scheduler, latents, timest
     else:
         noisy_latents = noise_scheduler.add_noise(latents, noise, timesteps)
 
-    return noise, noisy_latents, timesteps
+    return noise, noisy_latents, timesteps, noise_offset
 
 
 def append_lr_to_logs(logs, lr_scheduler, optimizer_type, including_unet=True):
