04ba384051fc3ed82f46745933a065c2c5dae624
Diodotos <165386228+diodotosml@users.noreply.github.com>
2024-04-04 19:05:40 +0200

    Breakpoint for debug

--- train_network.py
+++ train_network.py
@@ -984,6 +984,7 @@ class NetworkTrainer:
                         else:
                             target = noise
                         if args.reg_image_training:
+                            breakpoint()
                             loss = torch.nn.functional.mse_loss(noisy_latents.float() - noise_pred.float(), cond_latents.float(), reduction="none")
                         else:
                             loss = torch.nn.functional.mse_loss(noise_pred.float(), target.float(), reduction="none")
