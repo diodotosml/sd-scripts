3ebf1b91e3df037e2893b55f2445100bcdb0bf08
Diodotos <165386228+diodotosml@users.noreply.github.com>
2024-07-04 13:37:45 +0200

    Add debug log

--- train_network.py
+++ train_network.py
@@ -1033,6 +1033,9 @@ class NetworkTrainer:
                         min_timestep = batch["min_timestep"] if args.min_timestep is None else args.min_timestep
                         max_timestep = batch["max_timestep"] if args.max_timestep is None else args.max_timestep
 
+                        print(min_timestep)
+                        print(max_timestep)
+
                         noise, noisy_latents, timesteps,huber_c = train_util.get_noise_noisy_latents_and_timesteps_with_timestep_input(
                             args, noise_scheduler, latents, min_timestep, max_timestep
                         )
